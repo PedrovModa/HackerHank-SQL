5f465932f27e68b4c2a07b691605f80508fdab91
Pedro Moda <103860010+PedrovModa@users.noreply.github.com>
2022-08-24 15:04:14 -0300

    Update Select All.sql

--- SQL/Selects/Select All.sql
+++ SQL/Selects/Select All.sql
@@ -2,13 +2,8 @@ Query all columns (attributes) for every row in the CITY table.
 
 The CITY table is described as follows:
 
-| Field | Type |
-| --- | --- |
-| ID | NUMBER |
-| NAME | VARCHAR2(17) |
-| COUNTRY CODE | VARCHAR2(3) |
-| DISTRICT | VARCHAR2(20) |
-| POPULATION | NUMBER |
+| NOME | SOBRENOME |
+| PEDRO | MODA |
 
 SOLUÇÃO
 
