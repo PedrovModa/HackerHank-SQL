09c61a680291ee02b91de8026dab422051b9cb92
Pedro Moda <103860010+PedrovModa@users.noreply.github.com>
2022-08-24 15:12:08 -0300

    Update Revising the Select Query I.sql

--- SQL/Selects/Revising the Select Query I.sql
+++ SQL/Selects/Revising the Select Query I.sql
@@ -2,19 +2,18 @@ Query all columns for all American cities in the CITY table with populations lar
 
 The CITY table is described as follows:
 
-+-------------+--------------+
-| Field       | Type         |
-+-------------+--------------+
-| ID          | NUMBER       |
-| NAME        | VARCHAR2(17) |
-| COUNTRYCODE | VARCHAR2(3)  |
-| DISTRICT    | VARCHAR2(20) |
-| POPULATION  | NUMBER       |
-+-------------+--------------+
+|  Field | Type |
+|-------|-----|
+| ID  | NUMBER |
+| NAME | VARCHAR2(17)   |
+| COUNTRY CODE  | VARCHAR2(3)  |
+| DISTRICT |  VARCHAR2(20) |
+| POPULATION | NUMBER |
 
-RESPOSTA:
+**SOLUÇÃO**
 
+```sql
 SELECT *
 FROM CITY
 WHERE POPULATION > '100000' AND COUNTRYCODE = 'USA'
-
+```
