2a88433023b5b062e5b6727535725bf979a2b241
Pedro Moda <103860010+PedrovModa@users.noreply.github.com>
2022-08-24 14:53:52 -0300

    Update Select All.sql

--- SQL/Selects/Select All.sql
+++ SQL/Selects/Select All.sql
@@ -2,15 +2,13 @@ Query all columns (attributes) for every row in the CITY table.
 
 The CITY table is described as follows:
 
-+-------------+----------+
-| Field       | Type     |
-+-------------+----------+
-| ID          | int(11)  |
-| Name        | char(35) |
-| CountryCode | char(3)  |
-| District    | char(20) |
-| Population  | int(11)  |
-+-------------+----------+
+|  Field | Type |
+|---|---|
+| ID  | NUMBER |
+| NAME | VARCHAR2(17)   |
+| COUNTRY CODE  | VARCHAR2(3)  |
+| DISTRICT |  VARCHAR2(20) |
+| POPULATION | NUMBER |
 
 RESULTADO:
 
